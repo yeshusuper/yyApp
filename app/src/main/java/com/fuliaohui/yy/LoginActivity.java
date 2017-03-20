7e192de60d1c8b33ecd479ad10c6ca1d0bed8fad
lam <yeshusuper@qq.com>
2017-03-20 22:06:13 +0800

    登陆界面完成

---
+++ app/src/main/java/com/fuliaohui/yy/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.fuliaohui.yy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by lam on 2017/3/20.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final View loginContainer = findViewById(R.id.ll_login_container);
+        final View infoContainer = findViewById(R.id.ll_info_container);
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginContainer.setVisibility(View.GONE);
+                infoContainer.setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/fuliaohui/yy/MainActivity.java
+++ app/src/main/java/com/fuliaohui/yy/MainActivity.java
@@ -1,7 +1,11 @@
 package com.fuliaohui.yy;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.fuliaohui.yy.fragments.ClassSelecterFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Fragment classSelecterFragment = new ClassSelecterFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fl_fragment, classSelecterFragment)
+                .show(classSelecterFragment)
+                .commit();
     }
 }
