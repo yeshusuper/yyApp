f017ffc6eb1042157bc4a73c1fef42ae4a6cd5e1
lam <yeshusuper@qq.com>
2017-03-22 21:31:29 +0800

    增加新品分类选择

--- app/src/main/java/com/fuliaohui/yy/fragments/RightClassSelecterDialogFragment.java
+++ app/src/main/java/com/fuliaohui/yy/fragments/RightClassSelecterDialogFragment.java
@@ -3,7 +3,6 @@ package com.fuliaohui.yy.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,21 +10,26 @@ import android.view.ViewGroup;
 import com.fuliaohui.yy.R;
 
 /**
- * Created by lam on 2017/3/21.
+ * Created by lam on 2017/3/22.
  */
 
-public class NewProductFragment extends Fragment {
+public class RightClassSelecterDialogFragment extends DialogFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_FRAME, R.style.DialogTheme);
+        setCancelable(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_new_product, null);
-        view.findViewById(R.id.ll_select_btn)
+        final View view = inflater.inflate(R.layout.fragment_right_class_selecter, null);
+        view.findViewById(R.id.fl_bg)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        String name = RightClassSelecterDialogFragment.class.getName();
-                        DialogFragment fragment = (DialogFragment) DialogFragment.instantiate(getContext(), name);
-                        fragment.show(getChildFragmentManager(), name);
+                        dismiss();
                     }
                 });
         return view;
